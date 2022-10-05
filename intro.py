65c5f88fb63b15a89099175cb164c52dc33987c5
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2022-10-05 10:44:40 -0400

    add translation

--- intro.py
+++ intro.py
@@ -1,3 +1,5 @@
+import turtle
+
 """
 Translate this from js to Python
 let names = ['Angelo', 'Corey', 'Scottie']
@@ -5,9 +7,9 @@ for (let name of names){
     console.log(`Hello, ${name}`)
 }
 """
-names = ['Angelo', 'Corey', 'Scottie']
-for name in names:
-    print(f"Hello {name}!")
+# names = ['Angelo', 'Corey', 'Scottie']
+# for name in names:
+#     print(f"Hello {name}!")
     
 """
 https://codepen.io/rlconley/pen/PoRpxaP?editors=0010
@@ -27,12 +29,47 @@ while (index < names.length) {
   index++;
 }
 """
-total_chars = 0
-index = 0
+# total_chars = 0
+# index = 0
 
-while index < len(names):
-    total_chars += len(names[index])
-    print(f"Name: {names[index]}, index: {index}")
-    index += 1
+# while index < len(names):
+#     total_chars += len(names[index])
+#     print(f"Name: {names[index]}, index: {index}")
+#     index += 1
 
-print(f"The total number characters in all the names is {total_chars}")
+# print(f"The total number characters in all the names is {total_chars}")
+
+# turtle.getscreen()
+# franklin = turtle.Turtle()
+# for side in range(1, 4):
+#   franklin.forward(50)
+#   franklin.right(120)
+#   print(f"Drew side {side}")
+# turtle.done()
+
+# in JavaScript, this is called an array, and I would use let or const
+# let blocks = ["Sarah", "April", "Anna", "Taylor"]
+# in Python, this is called a list and we don't use let or const
+blocks = ["Sarah", "April", "Anna", "Taylor"]
+# in JavaScript, if we wanted to add a block to the team total for 
+# each player
+"""
+let blocks = ["Sarah", "April", "Anna", "Taylor"]
+let team_total = 0
+for (let block of blocks){
+  team_total += 1
+}
+"""
+
+blocks = ["Sarah", "April", "Anna", "Taylor"]
+team_total = 0
+for block in blocks:
+  team_total += 1
+
+
+
+
+
+
+
+
