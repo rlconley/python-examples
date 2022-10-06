4881f718f3125a44accbe5e85fa73a89721d13a0
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2022-10-06 10:27:59 -0400

    through variables and types

---
+++ concept_practice.py
@@ -0,0 +1,43 @@
+# Values and Variables
+# Numbers (Integers and Floats)
+a = 3
+b = 5.79
+c = a + b
+d = int(b)
+e = round(-3.5)
+f = 7 % 2
+print(type(a))
+print(type(b))
+print(type(c), c)
+print(d)
+print(e)
+print(f)
+
+# Strings
+print('Hello, World! \n Hello, World!')
+print('Hello, world!', 'Hello, world!')
+print('Hello, world!' + 'Hello, world!')
+
+# Booleans
+# capitalized in Python
+password = 'password123'
+print('Password123' == password)
+print(5 < 7)
+print('6' != 6)
+print(1 == '1' and 'hello' == 'hello')
+print(1 == '1' and 'hello' == 'hello')
+print(1 == '1' or 'hello' == 'hello')
+
+# Variables
+cooper_household_cats = 4
+print(type(cooper_household_cats))
+
+# Input and Output
+# input stored as string unless you change it
+dogs = int(input("How many dogs do you have? "))
+print(type(dogs))
+
+# Example
+# check length of password to make sure they are long enough
+password = input("Enter your password: ")
+print(len(password) >= 8)
